4d9955daa072b2fc6827ab24d530c5ebb125ee72
Cinxgler Mariaca M <cinxgler@gmail.com>
2012-10-22 19:21:43 -0500

    added Ocs.OpenLayers.Control.SinglePointEditingToolbar

--- src/js/ocs-public-portal.js
+++ src/js/ocs-public-portal.js
@@ -32,3 +32,70 @@ Ocs.Map = Backbone.Model.extend({
         this.map.render(element);
     },
 });
+
+Ocs.OpenLayers = {};
+Ocs.OpenLayers.Control = {};
+Ocs.OpenLayers.Control.SinglePointEditingToolbar = OpenLayers.Class( OpenLayers.Control.Panel, {
+    /**
+        * APIProperty: citeCompliant
+        * {Boolean} If set to true, coordinates of features drawn in a map extent
+        * crossing the date line won't exceed the world bounds. Default is false.
+        */
+    citeCompliant: false,
+
+    /**
+     * Constructor: Ocs.OpenLayers.Control.SinglePointEditingToolbar
+        * Create an editing toolbar for a given layer.
+        *
+        * Parameters:
+        * layer - {<OpenLayers.Layer.Vector>}
+        * options - {Object}
+        */
+    initialize: function(layer, options) {
+        OpenLayers.Control.Panel.prototype.initialize.apply(this, [options]);
+
+        this.addControls(
+            [ new OpenLayers.Control.Navigation() ]
+        );
+        var controls = [
+        new Ocs.OpenLayers.Control.DrawOnePointOnly(layer, OpenLayers.Handler.Point, {
+            displayClass: 'olControlDrawFeaturePoint',
+            handlerOptions: {citeCompliant: this.citeCompliant}
+        }),
+        ];
+        this.addControls(controls);
+    },
+
+    /**
+        * Method: draw
+        * calls the default draw, and then activates mouse defaults.
+        *
+        * Returns:
+        * {DOMElement}
+        */
+    draw: function() {
+        var div = OpenLayers.Control.Panel.prototype.draw.apply(this, arguments);
+        if (this.defaultControl === null) {
+            this.defaultControl = this.controls[1];
+        }
+        return div;
+    },
+
+    CLASS_NAME: "OpenLayers.Control.EditingToolbar"
+});
+
+Ocs.OpenLayers.Control.DrawOnePointOnly = OpenLayers.Class( OpenLayers.Control.DrawFeature, {
+    initialize: function(layer, handler, options) {
+        OpenLayers.Control.DrawFeature.prototype.initialize.apply(this, [layer, handler, options]);
+    },
+
+    /**
+     * Method: drawFeature
+     */
+    drawFeature: function(geometry) {
+        this.layer.removeAllFeatures();
+        OpenLayers.Control.DrawFeature.prototype.drawFeature.apply(this, [geometry]);
+    },
+
+    CLASS_NAME: "OpenLayers.Control.DrawFeature"
+});
