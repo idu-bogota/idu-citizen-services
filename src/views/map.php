14a4b94c21e5f0969d2a8d599a5c9a3efc868d6f
Cinxgler Mariaca M <cinxgler@gmail.com>
2012-11-02 19:54:56 -0500

    added script to hide/show citizen fields and changed marker style

--- src/views/map.php
+++ src/views/map.php
@@ -1,20 +1,45 @@
-<div class="span5">
-    <?php echo $form->render(new Zend_View()) ?>
-</div>
 <div class="span6">
     <fieldset>
         <legend>Ubicación</legend>
-        <div id='map_element' class="map"></div>
+        <div id='map_element' class="map span12"></div>
     </fieldset>
 </div>
-
+<div class="span6">
+    <?php echo $form->render(new Zend_View()) ?>
+</div>
 <script type='text/javascript'>
     $(document).ready(function(){
+        var styleMap = new OpenLayers.StyleMap({
+            'default': {
+                pointRadius: 10,
+                fillOpacity: 1,
+                externalGraphic: 'http://www.openlayers.org/dev/img/marker.png'
+            },
+            'temporary': {
+                fillOpacity: 0.5,
+                pointRadius: 10,
+                externalGraphic: 'http://www.openlayers.org/dev/img/marker.png'
+            }
+        });
+
         window.main = new Ocs.View.Map({
-            model: new Ocs.Model.Map(),
+            model: new Ocs.Model.Map({
+              markers: new OpenLayers.Layer.Vector("Markers", {styleMap: styleMap})
+            }),
             initial_zoom: 15,
             el: $('#map_element')
         });
         main.render();
+
+        var is_anonymous_checked = $('#is_anonymous').attr('checked')?true:false;
+        if(is_anonymous_checked) {
+            $("#fieldset-datos_contacto").hide();
+            $("#fieldset-datos_personales").hide();
+        }
+
+        $('#is_anonymous').click(function () {
+            $("#fieldset-datos_contacto").toggle();
+            $("#fieldset-datos_personales").toggle();
+        });
     });
 </script>
