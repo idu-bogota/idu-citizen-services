127c64f1d90f2e26239f8f1e4f7e73d6a6536aa5
Cinxgler Mariaca M <cinxgler@gmail.com>
2013-01-14 19:20:47 -0500

    display error message on attachement error

--- src/views/wizard_descripcion.php
+++ src/views/wizard_descripcion.php
@@ -9,17 +9,28 @@
   </div>
   <div class="span6">
     <?php if(!is_mobile()): ?>
+        <?php
+            $form_errors = $form->getMessages();
+            $image_errors_msg = false;
+            if(isset($form_errors['image'])) {
+                $image_errors = $form_errors['image'];
+                $image_errors_msg = join('. ', array_values($image_errors));
+            }
+        ?>
         <div class="fileupload fileupload-new" data-provides="fileupload">
             <div class="fileupload-new thumbnail" style="width: 200px; height: 150px;">
               <img src="http://www.placehold.it/200x150/EFEFEF/AAAAAA&text=Sin+imagen" />
             </div>
             <div class="fileupload-preview fileupload-exists thumbnail" style="max-width: 200px; max-height: 150px; line-height: 20px;"></div>
             <div>
+                <?php if($image_errors_msg): ?>
+                    <span class="error"><?php echo $image_errors_msg ?></span>
+                <?php endif ?>
                 <span class="btn btn-file">
                     <span class="fileupload-new">Adjuntar una imagen</span>
                     <span class="fileupload-exists">Cambiar</span>
                     <input id="MAX_FILE_SIZE" type="hidden" value="2097152" name="MAX_FILE_SIZE">
-                    <input id="image" type="file" name="image">
+                    <input id="image" type="file" name="image" />
                 </span>
                 <a href="#" class="btn fileupload-exists" data-dismiss="fileupload">Eliminar</a>
             </div>
