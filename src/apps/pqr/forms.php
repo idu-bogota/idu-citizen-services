1b0a3cb788b2eb0f6ad20725e25d0c1a51b81d25
Cinxgler Mariaca M <cinxgler@gmail.com>
2013-06-14 18:12:25 -0500

    adicionado attachments

--- src/apps/pqr/forms.php
+++ src/apps/pqr/forms.php
@@ -17,6 +17,21 @@ class PqrForm extends BasePqrForm {
         foreach($config_map as $f) {
             $pqr->attributes[$f] = $$f;
         }
+        $upload = new Zend_File_Transfer_Adapter_Http();
+        $upload->receive();
+        $fname_uploaded = $upload->getFileName();
+        if(!empty($fname_uploaded)) {
+            $filemd5 = md5_file($fname_uploaded);
+            $filesize = filesize ($fname_uploaded);
+            $file_info = $upload->getFileInfo();
+            $file_encode64 = base64_encode(file_get_contents($fname_uploaded));
+            $pqr->attributes['attachment'] = $file_encode64;
+            $pqr->attributes['attachment_name'] = $file_info['attachment']['name'];
+            $pqr->attributes['description'] .= sprintf("\n\n------\nSe adjunta archivo: %s\n - Tamaño en bytes: %s\n - MD5 Hash: %s\n",
+                $pqr->attributes['attachment_name'], $filesize, $filemd5
+            );
+        }
+
         return $pqr;
     }
 }
