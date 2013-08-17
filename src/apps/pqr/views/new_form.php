6b7ea43e62cfacd42397af39953e4f14797bd588
Cinxgler Mariaca M <cinxgler@gmail.com>
2013-08-16 19:26:21 -0500

    adicionado validacion javascript

--- src/apps/pqr/views/new_form.php
+++ src/apps/pqr/views/new_form.php
@@ -1,1 +1,30 @@
 <?php echo $form->render($view) ?>
+<script type="text/javascript">
+    $(document).ready(function(){
+        $('form').submit(function() {
+            var mensajes = [];
+            if(!$('#description').val()) {
+                mensajes.push('Por favor ingrese una descripción');
+            }
+
+            if(($('#name').val() && !$('#lastname').val()) || (!$('#name').val() && $('#lastname').val())) {
+                mensajes.push('Por favor diligencie nombres y apellidos');
+            }
+
+            if( $('#name').val() && !(
+                $('#email').val() ||
+                $('#twitter').val() ||
+                $('#facebook').val() ||
+                $('#phone').val() ||
+                $('#address').val()
+            )) {
+                mensajes.push('Por favor ingrese al menos un dato de contacto');
+            }
+
+            if(mensajes.length > 0) {
+                alert(mensajes.join("\n"));
+                return false;
+            }
+        });
+    });
+</script>
